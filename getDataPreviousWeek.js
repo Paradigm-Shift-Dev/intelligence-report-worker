8f17b4202be278b244c1b78f293abb9e4a662dfa
Martin N. Gregersen <martin@paradigmshift.ee>
2023-12-03 17:51:21 +0100

    Create getDataPreviousWeek.js

---
+++ getDataPreviousWeek.js
@@ -0,0 +1,56 @@
+async function getData() {
+  // Get the current date
+  const currentDate = new Date();
+  // Get a date for 14 days ago
+  const fourteenDaysAgo = new Date(currentDate.getTime() - 14 * 24 * 60 * 60 * 1000);
+  // Get a date for 7 days ago
+  const sevenDaysAgo = new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000);
+  // Format dates to "YYYY-MM-DDTHH:mm:ss.sssZ"
+  const formattedFourteenDaysAgo = fourteenDaysAgo.toISOString();
+  const formattedSevenDaysAgo = sevenDaysAgo.toISOString();
+
+    const query = `
+      query {
+  listChatbots(filter: {
+      createdAt: {
+        gt: "${formattedFourteenDaysAgo}",
+        lt: "${formattedSevenDaysAgo}",
+      }
+  }) {
+      items {
+        question,
+        answer
+      }
+  }  
+}
+    `;
+
+    let results = await fetch('https://fpzurbf4rzew3aod2faq7xkpla.appsync-api.eu-central-1.amazonaws.com/graphql', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "da2-kgkbycmsandt7a3qfwvx76wj7e"
+      },
+      body: JSON.stringify({ query }),
+    }).then(r =>  r.json().then(data => ({status: r.status, body: data})))
+    .then(data => {
+      return data
+    });
+    let jobResponse = await results;
+    console.log(jobResponse);
+    let notificationJobs = await jobResponse.body.data.listChatbots.items;
+    return notificationJobs;
+  }
+  const chatbotData = await getData();
+  const concatenatedQuestions = chatbotData
+  .map(item => item.question)
+  .join(' ');
+const concatenatedAnswers = chatbotData
+  .map(item => item.question)
+  .join(' ');
+
+output = [{
+  chatbotData: chatbotData, 
+  concatenatedQuestions: concatenatedQuestions,
+  concatenatedAnswers: concatenatedAnswers
+}];
